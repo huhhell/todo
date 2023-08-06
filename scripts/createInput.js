ab9fc9bd9822a22a353b7bee59d18328d2cf1c24
huhhell <vika20020807@mail.ru>
2023-08-06 18:29:48 +0700

    add check if field exists

--- scripts/createInput.js
+++ scripts/createInput.js
@@ -1,16 +1,15 @@
-let addTaskButton = document.querySelector('.header__create-button')
+let addTaskButton = document.querySelector('.header__create-button');
 addTaskButton.onclick = function addTask() {
+    let input = document.querySelector('.input');
+    if (input !== null) return false;
     let header = document.querySelector('.header');
-    // let inputField = document.createElement('div');
-    // inputField.classList.add('input')
-    // inputField.innerHTML = ' <input type="text" class="input__field" id="input__field" autofocus> <button class="input__button"> <img src="../img/addTask.svg" alt="plus" class="input__button-img"> </button>';
 
     header.insertAdjacentHTML("afterend", '<div class="input">\n' +
-        '        <label for="input__field"></label>\n' +
-        '        <input type="text" class="input__field" id="input__field" autoFocus>\n' +
-        '            <button class="input__button">\n' +
-        '                <img src="img/addTask.svg" alt="plus" class="input__button-img">\n' +
-        '            </button>\n' +
+        '        <label for="input__field" class="input__label"></label>\n' +
+        '        <input type="text" class="input__field" id="input__field" autofocus>\n' +
+        '        <button class="input__button">\n' +
+        '            <img src="img/addTask.svg" alt="plus" class="input__button-img">\n' +
+        '        </button>\n' +
         '    </div>')
 };
 

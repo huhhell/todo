fb471a593b8bcc38e13dc42e2a67b99a55c8d803
huhhell <vika20020807@mail.ru>
2023-08-07 16:32:37 +0700

    improve button function

--- scripts/submitInput.js
+++ scripts/submitInput.js
@@ -5,12 +5,17 @@ function submitTask() {
     if (inputField.value === '') return false;
 
     header.insertAdjacentHTML("afterend", '    <div class="task">\n' +
-        '        <div class="task__text">Cleaning dinner room</div>\n' +
-        '        <button class="task__done" onclick="doTask()">\n' +
+        '        <div class="task__text"></div>\n' +
+        '        <button class="task__done" onclick="doTask(this)">\n' +
         '            <img src="img/tick.svg" alt="tick" class="task__done-img">\n' +
         '        </button>\n' +
         '    </div>');
+
     let taskText = document.querySelector('.task__text');
     taskText.innerHTML = inputField.value;
+
+    let doneField = document.querySelector('.task__done');
+    doneField.setAttribute('isDone', 'false')
+
     input.remove();
 }
--- scripts/taskDone.js
+++ scripts/taskDone.js
@@ -1,15 +1,15 @@
-let isDone = false;
 
-function doTask() {
-    let doneField = document.querySelector('.task__done');
-    let doneTick = document.querySelector('.task__done-img');
-    if (isDone === false) {
-        doneField.style.backgroundColor = 'rgba(233,233,233,0)';
+function doTask(item) {
+    let doneTick = item.querySelector('*');
+    let isDone = item.getAttribute('isDone');
+
+    if (isDone === 'false') {
+        item.style.backgroundColor = 'rgba(233,233,233,0)';
         doneTick.style.display = 'block';
-        isDone = true;
+        item.setAttribute('isDone', 'true')
     } else {
-        doneField.style.backgroundColor = 'rgb(233,233,233)';
+        item.style.backgroundColor = 'rgb(233,233,233)';
         doneTick.style.display = 'none';
-        isDone = false;
+        item.setAttribute('isDone', 'false')
     }
 }
